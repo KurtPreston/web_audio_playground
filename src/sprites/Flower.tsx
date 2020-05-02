f89f28b5d154f73be827cb4892738292e4e64b20
Kurt Preston <github@kurtpreston.com>
2020-05-02 02:37:01 -0500

    Pulling circularPath logic out of Flower

--- src/sprites/Flower.tsx
+++ src/sprites/Flower.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {randomWalkFactory} from '../frameTickers/randomWalk';
 import {Dimensions, IWanderer, SpriteTicker, WorldState} from '../types';
 import {scale} from '../util/scale';
+import {circularPath} from './renderHelpers/circularPath';
 import {Sprite} from './Sprite';
 
 export class Flower extends Sprite {
@@ -31,8 +32,8 @@ export class Flower extends Sprite {
 
   public render(world: WorldState): React.ReactElement<SVGElement> {
     const {audio} = world;
-    const amplitude = audio.amplitude;
-    const size = scale({
+    const {amplitude, frequencies} = audio;
+    const maxFlowerSize = scale({
       input: amplitude,
       inputMin: 0,
       inputMax: 1,
@@ -41,57 +42,17 @@ export class Flower extends Sprite {
       logarithmic: true
     });
 
-    const pathCoords: {x: number; y: number}[] = new Array(audio.frequencies.length);
-    audio.frequencies.forEach((value: number, idx: number) => {
-      pathCoords[idx] = {
-        x: idx,
-        y: value
-      };
-    });
-
-    // const frequencyMeter = `M0,0, ${pathCoords.map(({x, y}) => `L${x},${y}`)}`;
-    // visualizer = <path d={frequencyMeter} style={{stroke: 'white'}}/>;
-
-    const frequencyGroups = chunk(pathCoords, pathCoords.length / this.numFlowers);
+    const frequencyGroups = chunk(frequencies, frequencies.length / this.numFlowers);
     const flowerRings = frequencyGroups.map((group, groupIdx) => {
-      const circularCoords = group.map(({y}, idx) => {
-        const r = scale({
-          input: y,
-          inputMin: 0,
-          inputMax: 255,
-          outputMin: 0,
-          outputMax: size
-        });
-        const angle = scale({
-          input: idx,
-          inputMin: 0,
-          inputMax: group.length - 1,
-          outputMin: 0,
-          outputMax: 2 * Math.PI
-        });
-        const circularX = Math.cos(angle) * r + this.state.x;
-        const circularY = Math.sin(angle) * r + this.state.y;
-        return {
-          x: circularX,
-          y: circularY
-        };
+      return circularPath({
+        cx: this.state.x,
+        cy: this.state.y,
+        minSize: 0,
+        maxSize: maxFlowerSize,
+        className: 'flower',
+        key: groupIdx,
+        wave: group
       });
-
-      const last = circularCoords[circularCoords.length - 1];
-      const circularFrequencyMeter = [
-        `M${last.x},${last.y}`,
-        ...circularCoords.map(({x, y}) => {
-          return `L${x},${y}`;
-        })
-      ].join(' ');
-      return (
-        <path
-          key={groupIdx}
-          className='flower'
-          d={circularFrequencyMeter}
-          style={{stroke: 'white'}}
-        />
-      );
     });
 
     return <g key={this.id}>{flowerRings}</g>;
---
+++ src/sprites/renderHelpers/circularPath.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {scale} from '../../util/scale';
+
+interface CircularPathParams {
+  cx: number;
+  cy: number;
+  wave: Uint8Array | number[];
+  minSize: number;
+  maxSize: number;
+  key: string | number;
+  className?: string;
+}
+
+export function circularPath(params: CircularPathParams): React.ReactElement<SVGPathElement> {
+  const {className, cx, cy, key, minSize, maxSize, wave} = params;
+
+  const coords: {x: number; y: number}[] = new Array(wave.length);
+
+  params.wave.forEach((amplitude: number, idx: number) => {
+    const r = scale({
+      input: amplitude,
+      inputMin: 0,
+      inputMax: 255,
+      outputMin: minSize,
+      outputMax: maxSize
+    });
+    const angle = scale({
+      input: idx,
+      inputMin: 0,
+      inputMax: wave.length - 1,
+      outputMin: 0,
+      outputMax: 2 * Math.PI
+    });
+    const circularX = Math.cos(angle) * r + cx;
+    const circularY = Math.sin(angle) * r + cy;
+    coords[idx] = {
+      x: circularX,
+      y: circularY
+    };
+  });
+
+  const last = coords[coords.length - 1];
+  const circularFrequencyMeter = [
+    `M${last.x},${last.y}`,
+    ...coords.map(({x, y}) => {
+      return `L${x},${y}`;
+    })
+  ].join(' ');
+  return (
+    <path key={key} className={className} d={circularFrequencyMeter} style={{stroke: 'white'}} />
+  );
+}
