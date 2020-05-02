fea8c3b4cd2efea983a3c9e27484c50438568233
Kurt Preston <github@kurtpreston.com>
2020-05-02 02:17:01 -0500

    Basic charging

--- src/sprites/Ryu.tsx
+++ src/sprites/Ryu.tsx
@@ -11,6 +11,7 @@ interface RyuState {
   x: number;
   y: number;
   chargeSize: number | null;
+  lastAmplitude: number;
 }
 
 export class Ryu extends Sprite {
@@ -24,7 +25,8 @@ export class Ryu extends Sprite {
     this.state = {
       x: params.dimensions.width / 2,
       y: params.dimensions.height - 50,
-      chargeSize: null
+      chargeSize: null,
+      lastAmplitude: 0
     };
   }
 
@@ -39,17 +41,35 @@ export class Ryu extends Sprite {
   }
 
   public tick(world: WorldState) {
-    const chargeSize = scale({
-      input: world.audio.amplitude,
-      inputMin: 0,
-      inputMax: 1,
-      outputMin: this.chargeMinSize,
-      outputMax: this.chargeMaxSize,
-      logarithmic: true
-    });
+    const shrinkRate = 5;
+    const growthRate = 10;
+    const amplitudeThreshold = 0.1;
+    const soundAmplitude = world.audio.amplitude - amplitudeThreshold;
+
+    const amplitudeDelta = soundAmplitude - this.state.lastAmplitude;
+
+    const growth =
+      amplitudeDelta >= 0
+        ? scale({
+            input: soundAmplitude,
+            inputMin: 0,
+            inputMax: 1,
+            outputMin: 0,
+            outputMax: growthRate
+          })
+        : scale({
+            input: amplitudeDelta,
+            inputMin: -1 * amplitudeThreshold,
+            inputMax: 0,
+            outputMin: -1 * shrinkRate,
+            outputMax: 0
+          });
+
+    const chargeSize = this.state.chargeSize ? this.state.chargeSize + growth : growth;
 
     this.state = {
       ...this.state,
+      lastAmplitude: soundAmplitude,
       chargeSize
     };
   }
