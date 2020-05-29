c7e8a4fee5683d31bc17afb59fe278afc647a460
Kurt Preston <github@kurtpreston.com>
2020-05-28 21:44:21 -0500

    Adding doppler shift

--- src/sprites/Microphone.ts
+++ src/sprites/Microphone.ts
@@ -1,7 +1,8 @@
+import {angleBetween} from '../math/trig/angleBetween';
 import {distanceBetween} from '../math/trig/distanceBetween';
 import {IPosition, WorldState} from '../types';
 import {midiNoteToFreq} from '../util/midi';
-import {scale} from '../util/scale';
+import {OverflowMode, scale} from '../util/scale';
 import {NoteNode} from './NoteGraph';
 import {Sprite} from './Sprite';
 
@@ -17,6 +18,7 @@ export class Microphone extends Sprite {
   private readonly noteNodes = new Set<NoteNode>();
   private readonly color = 'white';
   private readonly maxDistance = 600;
+  private readonly speedOfSound = 700;
 
   constructor(params: MicrophoneParams) {
     super();
@@ -41,19 +43,27 @@ export class Microphone extends Sprite {
 
     // Play the audio
     this.noteNodes.forEach((noteNode: NoteNode) => {
-      const {note, position: nodePosition, synth} = noteNode;
+      const {note, position: nodePosition, synth, vector} = noteNode;
+      const {xMomentum, yMomentum} = vector;
       const freq = midiNoteToFreq(note);
       const distanceToNode = distanceBetween(position, nodePosition);
+      const trajectoryAngle = angleBetween({x: 0, y: 0}, {x: xMomentum, y: yMomentum});
+      const angleToNode = angleBetween(nodePosition, position);
+      const angleDiff = trajectoryAngle - angleToNode;
+      const velocity = Math.sqrt(Math.pow(xMomentum, 2) + Math.pow(yMomentum, 2));
+      const velocityTowardNode = Math.cos(angleDiff) * velocity;
+      const adjustedFreq = (freq * (this.speedOfSound - velocityTowardNode)) / this.speedOfSound;
       const volume = scale({
         input: distanceToNode,
         inputMin: 0,
         inputMax: this.maxDistance,
         outputMin: 0,
-        outputMax: -100,
-        logarithmic: true
+        outputMax: -75,
+        logarithmic: true,
+        overflowMode: OverflowMode.Constrain
       });
       synth.volume.value = volume;
-      synth.triggerAttack(freq);
+      synth.triggerAttack(adjustedFreq);
     });
   }
 
