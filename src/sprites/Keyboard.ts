962129faace742e9b3a1e3a2000187db85279a99
Kurt Preston <github@kurtpreston.com>
2020-06-17 00:22:45 -0500

    Thinner black keys

--- src/sprites/Keyboard.ts
+++ src/sprites/Keyboard.ts
@@ -20,7 +20,7 @@ export class Keyboard implements Sprite {
     const notes: Note[] = range(21, 108);
     const numWhiteKeys = notes.filter((note) => !noteAccidental(note)).length;
 
-    const keyWidth = width / numWhiteKeys;
+    const whiteKeyWidth = width / numWhiteKeys;
 
     canvas.textAlign = 'center';
 
@@ -34,22 +34,24 @@ export class Keyboard implements Sprite {
         const color = activeNotes.has(note) ? noteColor(note, 0.7) : 'white';
         canvas.fillStyle = color;
         canvas.strokeStyle = 'black';
-        canvas.fillRect(startX, startY, keyWidth, whiteKeyHeight);
-        canvas.strokeRect(startX, startY, keyWidth, whiteKeyHeight);
+        canvas.fillRect(startX, startY, whiteKeyWidth, whiteKeyHeight);
+        canvas.strokeRect(startX, startY, whiteKeyWidth, whiteKeyHeight);
 
         canvas.fillStyle = tinycolor(color).isDark() ? 'white' : 'black';
         canvas.fillText(
           `${letter}${accidental || ''}`,
-          startX + keyWidth / 2,
+          startX + whiteKeyWidth / 2,
           height - 10,
-          keyWidth
+          whiteKeyWidth
         );
 
-        startX += keyWidth;
+        startX += whiteKeyWidth;
       }
     });
 
     startX = 0;
+    const blackKeyWidthRatio = 3 / 4;
+    const blackKeyWidth = whiteKeyWidth * blackKeyWidthRatio;
     notes.forEach((note: Note) => {
       const {letter, accidental} = getNoteInfo(note, 'b');
       if (accidental) {
@@ -57,13 +59,12 @@ export class Keyboard implements Sprite {
         const color = activeNotes.has(note) ? noteColor(note, 0.7) : 'black';
         canvas.fillStyle = color;
         canvas.strokeStyle = 'white';
-        canvas.fillRect(startX - keyWidth / 2, startY, keyWidth, blackKeyHeight);
-        canvas.strokeRect(startX - keyWidth / 2, startY, keyWidth, blackKeyHeight);
+        canvas.fillRect(startX - blackKeyWidth / 2, startY, blackKeyWidth, blackKeyHeight);
 
         canvas.fillStyle = tinycolor(color).isDark() ? 'white' : 'black';
-        canvas.fillText(`${letter}${accidental || ''}`, startX, height - 40, keyWidth);
+        canvas.fillText(`${letter}${accidental || ''}`, startX, height - 40, blackKeyWidth);
       } else {
-        startX += keyWidth;
+        startX += whiteKeyWidth;
       }
     });
   }
