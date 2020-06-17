ecaa3495d8e12c4ad8ae811908061e6e82cacacd
Kurt Preston <github@kurtpreston.com>
2020-06-16 22:10:01 -0500

    Implementing AutoChordMidiSource

--- src/midi/sources/AutoChordMidiSource.ts
+++ src/midi/sources/AutoChordMidiSource.ts
@@ -1,21 +1,65 @@
-import {NoteValue} from '../../audio/Note';
+import {autobind} from 'core-decorators';
+import {random, sampleSize} from 'lodash';
+import {randomChord} from '../../audio/chords';
+import {generateRelatedChord} from '../../audio/harmony';
+import {Note, NoteValue} from '../../audio/Note';
 import {MidiNotePublish} from '../MidiNoteBus';
-import {IMidiSource, MidiSourceClass} from './MidiSource';
+import {IMidiSource} from './MidiSource';
 interface AutoChordMidiSourceOptions {}
 
-export const AutoChordMidiSource: MidiSourceClass = class implements IMidiSource {
+@autobind
+export class AutoChordMidiSource implements IMidiSource {
   public options: AutoChordMidiSourceOptions = {};
+  private chord = new Map<NoteValue, Note[]>();
+  private chordChangeInterval: NodeJS.Timeout;
 
-  constructor(publish: MidiNotePublish) {
-    publish({
-      note: NoteValue.C + 36,
-      velocity: 1
+  constructor(private readonly publish: MidiNotePublish) {
+    this.setChord(randomChord().notes);
+    this.chordChangeInterval = setInterval(this.loadRelatedChord, 1000);
+  }
+
+  private setChord(newChord: Set<NoteValue>) {
+    // Add new notes
+    newChord.forEach((noteValue: NoteValue) => {
+      if (!this.chord.has(noteValue)) {
+        const notes: Note[] = sampleSize(
+          [noteValue + 24, noteValue + 36, noteValue + 48, noteValue + 60],
+          random(1, 4)
+        );
+        this.chord.set(noteValue, notes);
+        notes.forEach((note: Note) => {
+          this.publish({
+            note,
+            velocity: 127
+          });
+        });
+      }
+    });
+
+    // Delete old notes
+    this.chord.forEach((notes: Note[], noteValue: NoteValue) => {
+      if (!newChord.has(noteValue)) {
+        this.chord.delete(noteValue);
+        notes.forEach((note: Note) => {
+          this.publish({
+            note,
+            velocity: 0
+          });
+        });
+      }
     });
   }
 
-  public destroy() {}
+  private loadRelatedChord() {
+    const currentChord = new Set<NoteValue>(this.chord.keys());
+    this.setChord(generateRelatedChord(currentChord).notes);
+  }
+
+  public destroy() {
+    clearInterval(this.chordChangeInterval);
+  }
 
   public menu(): React.ReactNode {
     return 'Auto Chord!';
   }
-};
+}
