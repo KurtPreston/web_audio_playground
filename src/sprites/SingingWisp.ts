756756080e2860d1c4d3ede350e2d62cba4b98f1
Kurt Preston <github@kurtpreston.com>
2021-01-03 21:16:44 -0600

    SingingWisp uses mic

--- src/sprites/SingingWisp.ts
+++ src/sprites/SingingWisp.ts
@@ -1,23 +1,25 @@
 import {random} from 'lodash';
-import {Oscillator, PanVol, ToneAudioNode} from 'tone';
+import {Oscillator} from 'tone';
 import {midiNoteToFreq} from '../audio/midi';
 import {randomSustainOscillator} from '../audio/oscillators';
 import {BounceOffEdge, IForce} from '../math/traveler/forces';
 import {updateTraveler} from '../math/traveler/updateTraveler';
 import {IPosition, ITraveler, IVector, WorldState} from '../types/State';
+import {Microphone} from './Microphone/Microphone';
+import {MicrophoneConnection} from './Microphone/MicrophoneConnection';
 import {NoteNode} from './NoteGraph/NoteGraph';
 import {Sprite} from './Sprite';
 
 export class SingingWisp implements Sprite, NoteNode {
   private traveler: ITraveler;
   private forces: IForce[] = [BounceOffEdge];
+  private readonly connection: MicrophoneConnection;
   private readonly color = 'white';
   public readonly note = random(36, 60);
   public readonly synth: Oscillator;
-  public readonly panVol: PanVol;
   public readonly size: number = 20;
 
-  constructor(channel: ToneAudioNode) {
+  constructor(mic: Microphone) {
     this.traveler = {
       position: {
         x: 0,
@@ -30,9 +32,14 @@ export class SingingWisp implements Sprite, NoteNode {
     };
     const freq = midiNoteToFreq(this.note);
     this.synth = new Oscillator(freq, randomSustainOscillator());
-    this.panVol = new PanVol();
-    this.synth.connect(this.panVol);
-    this.panVol.connect(channel);
+    this.synth.start();
+    this.connection = mic.connect({
+      sourceAudio: this.synth,
+      sourcePosition: () => this.traveler,
+      pitchBend: (ratio: number) => {
+        this.synth.frequency.value = freq * ratio;
+      }
+    });
   }
 
   public get position(): IPosition {
@@ -54,5 +61,10 @@ export class SingingWisp implements Sprite, NoteNode {
 
   public tick(world: WorldState) {
     updateTraveler(this.traveler, this.forces, world);
+    this.connection.tick();
+  }
+
+  public destroy() {
+    this.connection.destroy();
   }
 }
