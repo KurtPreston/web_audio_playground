a254e2eb898c3adefcb04e27acf059f30bc57bdc
Kurt Preston <github@kurtpreston.com>
2021-01-03 21:20:06 -0600

    Restoring other wandering beats

--- src/sprites/Beat/BeatSequencer.ts
+++ src/sprites/Beat/BeatSequencer.ts
@@ -20,12 +20,10 @@ export class BeatSequencer implements Sprite {
 
     // Snare
     const snare = new Player('/samples/snare.wav');
-    snare.volume.value = -10;
 
     // Hat
     const hat = new Player('/samples/hihat.wav');
     hat.playbackRate = 2;
-    hat.volume.value = -20;
 
     // Create wandering beats
     const kickWanderer = new WanderingBeat({
@@ -36,52 +34,23 @@ export class BeatSequencer implements Sprite {
       fireworkColor: 'white',
       mic
     });
-    // const hatWanderer = new WanderingBeat({
-    //   sample: hat,
-    //   pattern: '8n',
-    //   dimensions,
-    //   fireworkSize: 15,
-    //   fireworkColor: 'cyan'
-    // });
-    // const snareWanderer = new WanderingBeat({
-    //   sample: snare,
-    //   pattern: '2n',
-    //   dimensions,
-    //   fireworkSize: 35,
-    //   fireworkColor: 'turquoise'
-    // });
-    // this.beats = [kickWanderer, hatWanderer, snareWanderer];
-    this.beats = [kickWanderer];
-
-    // Set-up microphones
-    // const kickMic = new SpriteMicrophone({
-    //   audioSettings: params.audioSettings,
-    //   audioSource: kick,
-    //   channel,
-    //   sourcePosition: () => kickWanderer.head,
-    //   micPosition: () => astronaut.traveler.position
-    // });
-    // kick.connect(kickMic.channel);
-
-    // const hatMic = new SpriteMicrophone({
-    //   audioSettings: params.audioSettings,
-    //   audioSource: hat,
-    //   channel,
-    //   sourcePosition: () => hatWanderer.head,
-    //   micPosition: () => astronaut.traveler.position
-    // });
-    // hat.connect(hatMic.channel);
-
-    // const snareMic = new SpriteMicrophone({
-    //   audioSettings: params.audioSettings,
-    //   audioSource: hat,
-    //   channel,
-    //   sourcePosition: () => snareWanderer.head,
-    //   micPosition: () => astronaut.traveler.position
-    // });
-    // snare.connect(snareMic.channel);
-    // this.mics = [kickMic, hatMic, snareMic];
-    // this.mics = [kickMic];
+    const hatWanderer = new WanderingBeat({
+      sample: hat,
+      pattern: '8n',
+      dimensions,
+      fireworkSize: 15,
+      fireworkColor: 'cyan',
+      mic
+    });
+    const snareWanderer = new WanderingBeat({
+      sample: snare,
+      pattern: '2n',
+      dimensions,
+      fireworkSize: 35,
+      fireworkColor: 'turquoise',
+      mic
+    });
+    this.beats = [kickWanderer, hatWanderer, snareWanderer];
 
     // Start sequencer
     Transport.bpm.value = 80;
